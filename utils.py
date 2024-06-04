587213664fff922a4deecd0cc72677c21e9dc515
Enzo Onofre <enzoonofre50@gmail.com>
2024-06-04 10:58:57 -0300

    update

--- utils.py
+++ utils.py
@@ -1,32 +1,28 @@
-'''This module contains utility functions for the project'''
+"""This module contains utility functions for the project"""
+
 import mmh3
+from haystack import Document
 
 
-def get_unique_docs(dataset, unique_docs):
-    '''Get unique documents from dataset
-    
+def get_unique_docs(dataset, unique_docs: set):
+    """Get unique documents from dataset
     Args:
     dataset: list of dictionaries
-    unique_docs: set of document ids
-
     Returns:
-    docs: list of dictionaries
-    '''
+    docs: list of haystack.Document
+    """
     docs = list()
-    for i in dataset:
-        if i["context"] is not None:
-            document_id = mmh3.hash128(i["context"], signed=False)
-            if document_id not in unique_docs:
-                unique_docs.add(document_id)
-
-                document = {}
-                document['content'] = i['context']
-                document['id'] = document_id
-            
-                aux = {}
-                aux['context_title'] = i['context_title']
-                aux['context.id'] = i['context_id']
-                document['meta'] = aux
-
-                docs.append(document)
+    for doc in dataset:
+        if doc["context"] is not None and doc["context_id"] not in unique_docs:
+            unique_docs.add(doc["context_id"])
+            document = Document(
+                content=doc["context"],
+                meta={
+                    "title": doc["context_title"],
+                    "context_id": doc["context_id"],
+                    "url": doc["url"],
+                    "source": "QASports",
+                },
+            )
+            docs.append(document)
     return docs
